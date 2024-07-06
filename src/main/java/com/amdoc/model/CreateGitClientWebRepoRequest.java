3c1865179cd2aec3c1d138f7b7bbb942b06dc247
Tung Dinh <tungxd301@gmail.com>
2024-07-06 00:47:57 -0700

    Changes

---
+++ src/main/java/com/amdoc/model/CreateGitClientWebRepoRequest.java
@@ -0,0 +1,16 @@
+package com.amdoc.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Value;
+import lombok.experimental.NonFinal;
+import lombok.extern.jackson.Jacksonized;
+
+@Value
+@NonFinal
+@Builder(toBuilder = true)
+@Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CreateGitClientWebRepoRequest {
+    String name;
+}
