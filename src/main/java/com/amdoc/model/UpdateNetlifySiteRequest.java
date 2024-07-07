cba57039f562356bc29aaca755d21011f09c010a
Tung Dinh <tungxd301@gmail.com>
2024-07-07 10:10:46 -0700

    Changes

---
+++ src/main/java/com/amdoc/model/UpdateNetlifySiteRequest.java
@@ -0,0 +1,20 @@
+package com.amdoc.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Value;
+import lombok.experimental.NonFinal;
+import lombok.extern.jackson.Jacksonized;
+
+@Value
+@NonFinal
+@Builder(toBuilder = true)
+@Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UpdateNetlifySiteRequest {
+    String siteId;
+    String domain;
+    String name;
+    String repo;
+    Long repoId;
+}
