8d561b33a92f69b38a71ef4246dd133f3e4f7dac
Tung Dinh <tungxd301@gmail.com>
2024-07-07 21:49:27 -0700

    Changes

---
+++ src/main/java/com/amdoc/model/UpdateAccountResponse.java
@@ -0,0 +1,17 @@
+package com.amdoc.model;
+
+import com.amdoc.pojo.DocAccount;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Value;
+import lombok.experimental.NonFinal;
+import lombok.extern.jackson.Jacksonized;
+
+@Value
+@NonFinal
+@Builder(toBuilder = true)
+@Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UpdateAccountResponse {
+    DocAccount account;
+}
